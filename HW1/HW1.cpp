a60ded626fe0512b5f04762a588f5971ce95510a
Dhruv Gupta <dhruvgupta22@iitk.ac.in>
2025-02-26 14:09:26 +0530

    Error while merginf resolved

--- HW1/HW1.cpp
+++ HW1/HW1.cpp
@@ -193,9 +193,6 @@ void MyExitRoutine() {
     memReadOp[0] = memReadOp[0]-memOp[0];
     memWriteOp[0] = memWriteOp[0]-memOp[0];
     
-    memReadOp[0] = memReadOp[0]-memOp[0];
-    memWriteOp[0] = memWriteOp[0]-memOp[0];
-    
     *out << "============================================================" << endl;
     *out << "Part A Analysis Results: " << endl;
     *out << "------------------------------------------------------------" << endl;
