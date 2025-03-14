8195274d882076a624bfc8719cbeecedd72a95bf
Dhruv Gupta <dhruvgupta22@iitk.ac.in>
2025-03-14 13:15:53 +0530

    GAg done

--- HW2/HW2.cpp
+++ HW2/HW2.cpp
@@ -10,27 +10,26 @@ using namespace std;
 
 /* Macro and type definitions */
 #define BILLION 1000000000
-#define MAX_INS_SIZE 20
-#define MAX_MEM_OPS 5
-#define MAX_INS_OPS 10
-#define MEM_LATENCY 70.0
 
 #define BIMODAL_ROW 512
 #define BIMODAL_COL 2
-#define BIMODAL_MID (1<<(BIMODAL_COL-1))
-#define BIMODAL_SIZE (1<<(BIMODAL_COL))
+#define BIMODAL_MID (1 << (BIMODAL_COL-1))
+#define BIMODAL_MAX ((1 << BIMODAL_COL)-1)
 
 #define SAg_BHT_ROW 1024
 #define SAg_BHT_COL 9
+#define SAg_BHT_MASK ((1 << SAg_BHT_COL)-1)
 #define SAg_PHT_ROW 512
 #define SAg_PHT_COL 2
-#define SAg_MID (1<<(SAg_PHT_COL-1))
-#define SAg_MAX (1<<(SAg_PHT_COL))
+#define SAg_MID (1 << (SAg_PHT_COL-1))
+#define SAg_MAX ((1 << (SAg_PHT_COL))-1)
 
-// unordered_map <ADDRINT, UINT64> pc_hash_bimodal;
-// UINT64 pht_index = 0;
-
-// #define NUM_HT_BUCKETS (1 << 20)  /* For footprint calculation */
+#define GAg_GHR_SIZE 9
+#define GAg_GHR_MASK ((1 << GAg_GHR_SIZE)-1)
+#define GAg_PHT_ROW 512
+#define GAg_PHT_COL 3
+#define GAg_MID (1 << (GAg_PHT_COL-1))
+#define GAg_MAX ((1 << (GAg_PHT_COL))-1)
 
 typedef enum{
     INS_DIRECT_CALL=0,
@@ -48,8 +47,9 @@ typedef enum{
     SAg,
     GAg,
     GSHARE,
-    HYBRID2,
-    HYBRID3,
+    COMB2,
+    COMB3MAJORITY,
+	COMB3,
     DP_COUNT
 } DIR_PRED;
 
@@ -65,6 +65,8 @@ UINT64 bimodal_pht[BIMODAL_ROW];
 UINT64 sag_bht[SAg_BHT_ROW];
 UINT64 sag_pht[SAg_PHT_ROW];
 
+UINT64 gag_ghr;
+UINT64 gag_pht[GAg_PHT_ROW];
 
 ADDRINT fastForwardDone = 0;
 UINT64 icount = 0; //number of dynamically executed instructions
@@ -109,14 +111,14 @@ ADDRINT Terminate(void){
 }
 
 
-string pred_names[3] = {
-	"FNBT", "Bimodal", "Sag"
+string pred_names[DP_COUNT] = {
+	"FNBT", "Bimodal", "SAg", "GAg", "gshare", "Combined2", "Combined3Majority", "Combined3"
 };
 
 VOID StatDump(void){
 	*out << "===============================================" << endl;
 	*out << "Direction Predictors" << endl;
-	for(int i=0; i<3; i++){
+	for(int i=0; i<DP_COUNT; i++){
 		*out << pred_names[i] << ": Accesses " << (dp_forwbr + dp_backbr);
 		*out << " Mispredictions " << (Mispred[i].forw + Mispred[i].back) ;
 		*out << " (" << (1.0*Mispred[i].forw + Mispred[i].back)/(1.0*dp_forwbr + dp_backbr) << ")";
@@ -127,7 +129,12 @@ VOID StatDump(void){
 		*out << " Backward Mispredictions " << Mispred[i].back;
 		*out << " (" << (1.0*Mispred[i].back)/(1.0*dp_backbr) << ")";
 		*out << endl;
-	}	
+	}
+	*out << endl;
+	*out << "Branch Target Predictors" << endl;
+	*out << endl;
+	*out << endl;
+	*out << "===============================================" << endl;
 	exit(0);
 }
 
@@ -147,50 +154,19 @@ VOID BkdMispred_FNBT(BOOL taken){
     Mispred[0].back += taken^1;
 }
 
-
-// VOID FwdMispred_bimodal(BOOL taken, ADDRINT ins_addr){
-// 	if(pc_hash_bimodal.find(ins_addr)==pc_hash_bimodal.end())
-//     {
-//         pht_index = (pht_index + 1)%BIMODAL_ROW;
-//         bimodal_pht[pht_index] = BIMODAL_MID;
-//         pc_hash_bimodal[ins_addr] = pht_index;
-//     }
-//     UINT64 ind = pc_hash_bimodal[ins_addr];
-// 	// UINT32 hpc = pc%BIMODAL_ROW;
-//     UINT8 pred = bimodal_pht[ind];
-// 	BOOL prediction = (pred < BIMODAL_MID)?0:1;
-// 	Mispred[1].forw += taken^prediction;
-// 	bimodal_pht[ind] = (taken) ? max(pred, (UINT8)(pred+1)) : min(pred, (UINT8)(pred-1));
-// }
-
-// VOID BkdMispred_bimodal(BOOL taken, ADDRINT ins_addr){
-// 	if(pc_hash_bimodal.find(ins_addr)==pc_hash_bimodal.end())
-//     {
-//         pht_index = (pht_index + 1)%BIMODAL_ROW;
-//         bimodal_pht[pht_index] = BIMODAL_MID;
-//         pc_hash_bimodal[ins_addr] = pht_index;
-//     }
-//     UINT64 ind = pc_hash_bimodal[ins_addr];
-// 	// UINT32 hpc = pc%BIMODAL_ROW;
-//     UINT8 pred = bimodal_pht[ind];
-// 	BOOL prediction = (pred < BIMODAL_MID)?0:1;
-// 	Mispred[1].back += taken^prediction;
-// 	bimodal_pht[ind] = (taken) ? max(pred, (UINT8)(pred+1)) : min(pred, (UINT8)(pred-1));
-// }
-
 VOID FwdMispred_bimodal(BOOL taken, UINT64 pc){
 	UINT64 hpc = pc%BIMODAL_ROW;
 	UINT64 pred = bimodal_pht[hpc];
 	BOOL prediction = (pred < BIMODAL_MID) ? 0 : 1;
 	Mispred[1].forw += taken^prediction;
-	bimodal_pht[hpc] = (taken) ? ((pred == BIMODAL_SIZE-1) ? pred : pred+1) : ((pred == 0) ? 0 : pred-1);
+	bimodal_pht[hpc] = (taken) ? ((pred == BIMODAL_MAX) ? pred : pred+1) : ((pred == 0) ? 0 : pred-1);
 }
 VOID BkdMispred_bimodal(BOOL taken, UINT64 pc){
 	UINT64 hpc = pc%BIMODAL_ROW;
 	UINT64 pred = bimodal_pht[hpc];
 	BOOL prediction = (pred < BIMODAL_MID) ? 0 : 1;
 	Mispred[1].back += taken^prediction;
-	bimodal_pht[hpc] = (taken) ? ((pred == BIMODAL_SIZE-1) ? pred : pred+1) : ((pred == 0) ? 0 : pred-1);
+	bimodal_pht[hpc] = (taken) ? ((pred == BIMODAL_MAX) ? pred : pred+1) : ((pred == 0) ? 0 : pred-1);
 }
 
 VOID FwdMispred_sag(BOOL taken, UINT64 pc){
@@ -199,8 +175,8 @@ VOID FwdMispred_sag(BOOL taken, UINT64 pc){
 	UINT64 counter = sag_pht[hist];
 	BOOL prediction = (counter < SAg_MID) ? 0 : 1;
 	Mispred[2].forw += taken^prediction;
-	sag_pht[hist] =  (taken) ? ((counter == SAg_MAX-1) ? counter : counter+1) : ((counter == 0) ? 0 : counter-1);
-	sag_bht[hpc] = ((hist << 1 | (taken)) & 0x1FF);
+	sag_pht[hist] =  (taken) ? ((counter == SAg_MAX) ? counter : counter+1) : ((counter == 0) ? 0 : counter-1);
+	sag_bht[hpc] = ((hist << 1 | (taken)) & SAg_BHT_MASK);
 }
 VOID BkdMispred_sag(BOOL taken, UINT64 pc){
 	UINT64 hpc = pc%SAg_BHT_ROW;
@@ -208,8 +184,25 @@ VOID BkdMispred_sag(BOOL taken, UINT64 pc){
 	UINT64 counter = sag_pht[hist];
 	BOOL prediction = (counter < SAg_MID) ? 0 : 1;
 	Mispred[2].back += taken^prediction;
-	sag_pht[hist] =  (taken) ? ((counter == SAg_MAX-1) ? counter : counter+1) : ((counter == 0) ? 0 : counter-1);
-	sag_bht[hpc] = ((hist << 1 | (taken)) & 0x1FF);
+	sag_pht[hist] =  (taken) ? ((counter == SAg_MAX) ? counter : counter+1) : ((counter == 0) ? 0 : counter-1);
+	sag_bht[hpc] = ((hist << 1 | (taken)) & SAg_BHT_MASK);
+}
+
+VOID FwdMispred_gag(BOOL taken){
+	UINT64 hist = gag_ghr & GAg_GHR_MASK;
+	UINT64 counter = gag_pht[hist];
+	BOOL prediction = (counter < GAg_MID) ? 0 : 1;
+	Mispred[3].forw += taken^prediction;
+	gag_pht[hist] =  (taken) ? ((counter == GAg_MAX) ? counter : counter+1) : ((counter == 0) ? 0 : counter-1);
+	gag_ghr = ((hist << 1 | (taken)) & GAg_GHR_MASK);
+}
+VOID BkdMispred_gag(BOOL taken){
+	UINT64 hist = gag_ghr & GAg_GHR_MASK;
+	UINT64 counter = gag_pht[hist];
+	BOOL prediction = (counter < GAg_MID) ? 0 : 1;
+	Mispred[3].back += taken^prediction;
+	gag_pht[hist] =  (taken) ? ((counter == GAg_MAX) ? counter : counter+1) : ((counter == 0) ? 0 : counter-1);
+	gag_ghr = ((hist << 1 | (taken)) & GAg_GHR_MASK);
 }
 
 /* Instruction instrumentation routine */
@@ -225,8 +218,6 @@ VOID Trace(TRACE trace, VOID *v)
 		BBL_InsertThenCall(bbl, IPOINT_BEFORE, (AFUNPTR) FastForwardDone, IARG_END);
 
 		for (INS ins = BBL_InsHead(bbl); ; ins = INS_Next(ins)) {
-			/* Instruction footprint */
-			ASSERT(INS_Size(ins) < MAX_INS_SIZE, "Instruction size exceeded MAX_INS_SIZE!");
 
 			/* Calculate instruction type */
 			switch (INS_Category(ins))
@@ -278,6 +269,8 @@ VOID Trace(TRACE trace, VOID *v)
 					INS_InsertIfCall(ins, IPOINT_BEFORE, (AFUNPTR) IsFastForwardDone, IARG_END);
 					INS_InsertThenCall(ins, IPOINT_BEFORE, (AFUNPTR) FwdMispred_sag, IARG_BRANCH_TAKEN, IARG_UINT64, (UINT64)ins_addr, IARG_END);
 
+					INS_InsertIfCall(ins, IPOINT_BEFORE, (AFUNPTR) IsFastForwardDone, IARG_END);
+					INS_InsertThenCall(ins, IPOINT_BEFORE, (AFUNPTR) FwdMispred_gag, IARG_BRANCH_TAKEN, IARG_END);
 				}
                 else{
                     INS_InsertIfCall(ins, IPOINT_BEFORE, (AFUNPTR) IsFastForwardDone, IARG_END);
@@ -292,6 +285,8 @@ VOID Trace(TRACE trace, VOID *v)
 					
 					INS_InsertIfCall(ins, IPOINT_BEFORE, (AFUNPTR) IsFastForwardDone, IARG_END);
 					INS_InsertThenCall(ins, IPOINT_BEFORE, (AFUNPTR) BkdMispred_sag, IARG_BRANCH_TAKEN, IARG_UINT64, (UINT64)ins_addr, IARG_END);
+					INS_InsertIfCall(ins, IPOINT_BEFORE, (AFUNPTR) IsFastForwardDone, IARG_END);
+					INS_InsertThenCall(ins, IPOINT_BEFORE, (AFUNPTR) BkdMispred_gag, IARG_BRANCH_TAKEN, IARG_END);
 				}
             }
 			// INS_InsertIfCall(ins, IPOINT_BEFORE, (AFUNPTR) IsFastForwardDone, IARG_END);
@@ -340,7 +335,8 @@ int main(int argc, char *argv[])
 	for(int i=0; i<BIMODAL_ROW; i++) bimodal_pht[i] = 0;
 	for(int i=0; i<SAg_BHT_ROW; i++) sag_bht[i] = 0;
 	for(int i=0; i<SAg_PHT_ROW; i++) sag_pht[i] = 0;
-
+	gag_ghr = 0;
+	for(int i=0; i<GAg_PHT_ROW; i++) gag_pht[i] = 0;
     
 	// Register function to be called to instrument instructions
 	TRACE_AddInstrumentFunction(Trace, 0);
